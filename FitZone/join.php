208d500f1977f7d51de547a2b7d63778c74b3a97
vampirevidha <vampirevidha@gmail.com>
2025-04-11 22:17:18 +0530

    Add FitZone/join.php 1744390038126

--- FitZone/join.php
+++ FitZone/join.php
@@ -4,6 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="assets/css/style.css">
+    <script src="assets/js/script.js" defer></script>
     <title>Join Us - FitZone</title>
 </head>
 <body>
@@ -11,7 +12,7 @@
         <h1>Join FitZone</h1>
     </header>
     <main>
-        <form action="submit_contact.php" method="post">
+        <form action="register_user.php" method="post">
             <label for="name">Name:</label>
             <input type="text" id="name" name="name" required>
             <label for="email">Email:</label>
