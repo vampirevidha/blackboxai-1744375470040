3e5445244bdb0adb721f93e3613609bf7c34aae0
vampirevidha <vampirevidha@gmail.com>
2025-04-11 22:17:14 +0530

    Add FitZone/logout.php 1744390033991

--- FitZone/logout.php
+++ FitZone/logout.php
@@ -1,6 +1,24 @@
 <?php
-session_start();
+require_once 'db/config.php';
+
+// Start the session if not already started
+if (session_status() == PHP_SESSION_NONE) {
+    session_start();
+}
+
+// Unset all session variables
+$_SESSION = array();
+
+// Destroy the session cookie
+if (isset($_COOKIE[session_name()])) {
+    setcookie(session_name(), '', time()-3600, '/');
+}
+
+// Destroy the session
 session_destroy();
+
+// Redirect to home page with logout message
+$_SESSION['success'] = "You have been successfully logged out.";
 header("Location: index.php");
 exit();
 ?>
